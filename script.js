0278474d9e83a67dfc2720f34b5f70f4e237bee8
StanleyHaw <stanleyhaw94@gmail.com>
2023-07-06 19:20:06 +0800

    added:footer and contact me

--- script.js
+++ script.js
@@ -80,3 +80,17 @@ function reveal() {
     }
   }
 }
+
+function SendMail() {
+  var params = {
+    from_name: document.getElementById("name").value,
+    company: document.getElementById("company").value,
+    email_id: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+  emailjs
+    .send("service_kcfbtp5", "template_63um3g3", params)
+    .then(function (res) {
+      alert("您已成功寄出留言到我的信箱咯！謝謝您，祝您事事順心！");
+    });
+}
