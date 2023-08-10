cd4dcd2e85391e908cf7e04defc523eaaf37623d
StanleyHaw <stanleyhaw94@gmail.com>
2023-08-10 14:24:28 +0800

    fixed:optimized js code

--- script.js
+++ script.js
@@ -1,69 +1,21 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   document.addEventListener("mousemove", (e) => {
-//     //傳入滑鼠位置
-//     document.querySelectorAll(".pos-3d").forEach((element) => {
-//       add3d(element, e);
-//     });
-//   });
-// });
+function displayNavbar() {
+  const navbar = document.querySelector(".navbar");
+  const scrollPosition = window.scrollY;
 
-// function add3d(element, e) {
-//   // 滑鼠位置
-//   const m_x = e.pageX;
-//   const m_y = e.pageY;
-
-//   const w_w = window.innerWidth,
-//     w_h = window.innerHeight,
-//     o_w = element.offsetWidth,
-//     distance = (element.offsetWidth - w_w) / 2,
-//     layer = Number(element.getAttribute("data-layer")),
-//     invert = Number(element.getAttribute("data-invert"));
-
-//   // 物件在頁面上的位置
-//   const o_x = w_w / 2;
-//   const o_y = w_h / 2;
-
-//   // 兩者的距離差
-//   let w_x = m_x - o_x;
-//   let w_y = m_y - o_y;
-
-//   // 距離相乘後開平方
-//   const w_wSqrt = Math.sqrt(w_w / 2);
-//   const d = distance / w_wSqrt / layer;
-
-//   // 距離計算
-//   let s_x = Math.sqrt(Math.abs(w_x));
-//   if (w_x < 0) {
-//     s_x = -s_x;
-//   }
-
-//   let d_x = s_x * d;
-//   // 加入樣式
-//   element.style.transform = `translateX(${invert ? d_x : -d_x}px)`;
-// }
-
-// 打字特效
-
-window.addEventListener("scroll", function () {
-  var navbar = document.querySelector(".navbar");
-  var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-
-  if (scrollPosition >= 1) {
-    navbar.style.top = "0px"; // 滚动位置大于等于 789px，显示导航栏
+  if (scrollPosition > 0) {
+    navbar.style.top = "0px";
   } else {
-    navbar.style.top = "-100px"; // 滚动位置小于 789px，隐藏导航栏
+    navbar.style.top = "-100px";
   }
-});
-
-window.addEventListener("scroll", reveal);
+}
 
 function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
+  const reveals = document.querySelectorAll(".reveal");
 
-  for (var i = 0; i < reveals.length; i++) {
-    var windowheight = window.innerHeight;
-    var revealtop = reveals[i].getBoundingClientRect().top;
-    var revealpoint = 150;
+  for (let i = 0; i < reveals.length; i++) {
+    const windowheight = window.innerHeight;
+    const revealtop = reveals[i].getBoundingClientRect().top;
+    const revealpoint = 150;
 
     if (revealtop < windowheight - revealpoint) {
       reveals[i].classList.add("active");
@@ -72,3 +24,8 @@ function reveal() {
     }
   }
 }
+
+window.addEventListener("scroll", function () {
+  displayNavbar();
+  reveal();
+});
